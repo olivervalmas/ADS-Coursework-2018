5bb1d9a39b0fac73d42f67b373782ba2b6bf3862
olivervalmas <oliver@valmas.co.uk>
2019-01-14 23:10:50 +0000

    completely changed approach to q2 (no recursion)

--- Q2/gpqk41_q2.py
+++ Q2/gpqk41_q2.py
@@ -1,7 +1,5 @@
 import time
 
-valid_sums = [[1, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58], [1, 4, 22, 19, 10, 25, 28, 31, 34, 37, 13, 40, 43, 16, 49, 52, 46], [1, 8, 30, 31, 35, 38, 41, 37]]
-
 
 def get_k_child(n, k):
 
@@ -14,58 +12,63 @@ def get_k_child(n, k):
     return total
 
 
-sequence = set()
-leads_to_ephemeral = set()
-leads_to_eternal = set()
-
-
-def is_ephemeral(n, k):
-
-    if sum(map(int, str(n))) not in valid_sums[k-2]:
-        sequence.clear()
-        return False
-
-    elif n in leads_to_eternal or n in sequence:
-
-        for i in sequence:
-            if i < 100000:
-                leads_to_eternal.add(i)
-
-        sequence.clear()
-        return False
-
-    if n == 1:
-        sequence.add(1)
-
-    if n in leads_to_ephemeral or n == 1:
-
-        for i in sequence:
-            if i < 100000:
-                leads_to_ephemeral.add(i)
-
-        sequence.clear()
-        return True
-
-    else:
-        sequence.add(n)
-        return is_ephemeral(get_k_child(n, k), k)
-
-
 def count_ephemeral(n1, n2, k):
 
+    ephemeral = set()
+    eternal = set()
+    sequence = set()
     count = 0
 
-    for i in range(n1, n2):
-        n_digit_sum = sum(map(int, str(i)))
+    valid_sums = [
+        [1, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+         32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58],
+        [1, 4, 22, 19, 10, 25, 28, 31, 34, 37, 13, 40, 43, 16, 49, 52, 46], [1, 8, 30, 31, 35, 38, 41, 37]]
 
-        if n_digit_sum in valid_sums[k - 2]:
-            if is_ephemeral(i, k):
+    for i in range(n1, n2):
+
+
+            duplicate = False
+
+            while i is not 1 and not duplicate:
+                i = get_k_child(i, k)
+
+                if i in eternal:
+                    duplicate = True
+
+                if i in ephemeral:
+                    i = 1
+
+                elif i in sequence:
+                    duplicate = True
+
+                else:
+                    sequence.add(i)
+
+            if not duplicate:
+                for number in sequence:
+                    ephemeral.add(number)
                 count += 1
+            else:
+                for number in sequence:
+                    eternal.add(number)
+
+            sequence.clear()
 
     return count
 
 
 start = time.time()
-print(count_ephemeral(1, 1000000, 2))
+print("Time for 1 to 10 million for k=2: " + str(count_ephemeral(1,10000000,2)))
 end = time.time()
 print(end-start)
+
+start = time.time()
+print("Time for 1 to 10 million for k=3: " + str(count_ephemeral(1,10000000,3)))
+end = time.time()
+print(end-start)
+
+start = time.time()
+print("Time for 1 to 10 million for k=4: " + str(count_ephemeral(1,10000000,4)))
+end = time.time()
+print(end-start)
+
